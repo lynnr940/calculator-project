0188f50a8942685ac7e950e71070dee5295fff01
Ryan Lynn <lynnr9940@gmail.com>
2023-08-16 15:59:00 -0400

    adds logic to make calculator function and display results

--- script.js
+++ script.js
@@ -1,46 +1,58 @@
 const numbers = document.querySelectorAll(".number");
 const operators = document.querySelectorAll(".operator");
-const AC = document.querySelector("#AC")
-const DEL = document.querySelector("#DEL")
-const EQUALS = document.querySelector("#=")
+const AC = document.querySelector("#AC");
+const DEL = document.querySelector("#DEL");
+const EQUALS = document.querySelector(".equals");
+const displayLower = document.querySelector(".lower");
+const displayUpper = document.querySelector(".upper");
 
 let num1 = "";
 let num2 = "";
 let result = "";
-let currentOperator = ""
-displayContent = ""
+let currentOperator = "";
+displayLower.textContent = "";
+displayUpper.textContent = "";
 let operatorEntered = false;
 
 numbers.forEach(function (i) {
   i.addEventListener("click", function () {
-   
+    if (!operatorEntered) {
+      num1 += i.value;
+      displayLower.textContent = num1;
+    } else {
+      num2 += i.value;
+      displayLower.textContent = num2;
+    }
   });
 });
 
 operators.forEach(function (i) {
   i.addEventListener("click", function () {
-   
+    currentOperator = i.value;
+    operatorEntered = true;
+    displayLower.textContent += currentOperator;
   });
 });
 
 AC.addEventListener("click", function () {
-   
+  num1 = "";
+  num2 = "";
+  currentOperator = "";
+  operatorEntered = false;
+  displayLower.textContent = "";
+  displayUpper.textContent = "";
 });
 
-DEL.addEventListener("click", function () {
-   
-});
+DEL.addEventListener("click", function () {});
+
 EQUALS.addEventListener("click", function () {
-   
+  console.log(operate(num1, currentOperator, num2));
+  result = operate(num1, currentOperator, num2);
+  displayUpper.textContent = result;
+  displayLower.textContent = "";
+  num1 = result;
+  num2 = "";
 });
-
-
-
-
-
-
-
-
 
 function add(num1, num2) {
   return num1 + num2;
@@ -60,20 +72,23 @@ function getRemainder(num1, num2) {
   return num1 % num2;
 }
 
-function operate(currentOperator, a, b) {
-  a = Number(a)
-  b = Number(b)
+function operate(a, currentOperator, b) {
+  console.log([a, currentOperator, b]);
+  a = Number(a);
+  b = Number(b);
   switch (currentOperator) {
-    case '+':
-      return add(a, b)
-    case '−':
-      return substract(a, b)
-    case '×':
-      return multiply(a, b)
-    case '÷':
-      if (b === 0) return null
-      else return divide(a, b)
+    case "+":
+      return add(a, b);
+    case "−":
+      return subtract(a, b);
+    case "×":
+      return multiply(a, b);
+    case "%":
+      return getRemainder(a, b);
+    case "÷":
+      if (b === 0) return null;
+      else return divide(a, b);
     default:
-      return null
+      return null;
   }
 }
